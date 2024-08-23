174c425dee7a6c4e668b64cc73d2dc98977feced
stuk-nastassia <nastalen7@gmail.com>
2024-08-23 16:43:49 +0300

    Add files via upload

---
+++ CrptApi.java
@@ -0,0 +1,336 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
+
+public class CrptApi {
+
+    private static final String API_URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+    private static final Logger logger = Logger.getLogger(CrptApi.class.getName());
+
+    private final HttpClient httpClient;
+    private final AtomicInteger requestCount;
+    private final ScheduledExecutorService scheduler;
+    private final Lock lock = new ReentrantLock();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final int requestLimit;
+
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        this.httpClient = HttpClient.newBuilder().build();
+        this.requestLimit = requestLimit;
+        long timeIntervalMillis = timeUnit.toMillis(1);
+        this.requestCount = new AtomicInteger(0);
+        this.scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        scheduler.scheduleAtFixedRate(() -> {
+            lock.lock();
+            try {
+                requestCount.set(0);
+            } finally {
+                lock.unlock();
+            }
+        }, timeIntervalMillis, timeIntervalMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public void createDocument(DocumentDto document, String signature) {
+        while (true) {
+            lock.lock();
+            try {
+                if (requestCount.get() < requestLimit) {
+                    requestCount.incrementAndGet();
+                    break;
+                }
+            } finally {
+                lock.unlock();
+            }
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                logger.warning("Поток был прерван во время ожидания следующего интервала запроса");
+            }
+        }
+
+        try {
+            String jsonRequest = toJson(document);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(API_URL))
+                    .header("Content-Type", "application/json")
+                    .header("Signature", signature)
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("Ошибка при отправке документа : " + response.body());
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Ошибка отправки запроса : " + e.getMessage());
+        }
+    }
+
+    private String toJson(DocumentDto document) throws IOException {
+        return objectMapper.writeValueAsString(document);
+    }
+
+    public void shutdown() {
+        scheduler.shutdown();
+    }
+
+    public static class DocumentDto {
+        @JsonProperty("doc_id")
+        private String docId;
+        @JsonProperty("doc_status")
+        private String docStatus;
+        @JsonProperty("doc_type")
+        private String docType;
+        @JsonProperty("importRequest")
+        private boolean importRequest;
+        @JsonProperty("owner_inn")
+        private String ownerInn;
+        @JsonProperty("participant_inn")
+        private String participantInn;
+        @JsonProperty("producer_inn")
+        private String producerInn;
+        @JsonProperty("production_date")
+        private String productionDate;
+        @JsonProperty("production_type")
+        private String productionType;
+        @JsonProperty("reg_date")
+        private String regDate;
+        @JsonProperty("reg_number")
+        private String regNumber;
+        @JsonProperty("products")
+        private List<Product> products;
+        @JsonProperty("description")
+        private Description description;
+
+        public String getDocId() {
+            return docId;
+        }
+
+        public void setDocId(String docId) {
+            this.docId = docId;
+        }
+
+        public String getDocStatus() {
+            return docStatus;
+        }
+
+        public void setDocStatus(String docStatus) {
+            this.docStatus = docStatus;
+        }
+
+        public String getDocType() {
+            return docType;
+        }
+
+        public void setDocType(String docType) {
+            this.docType = docType;
+        }
+
+        public boolean isImportRequest() {
+            return importRequest;
+        }
+
+        public void setImportRequest(boolean importRequest) {
+            this.importRequest = importRequest;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getProductionType() {
+            return productionType;
+        }
+
+        public void setProductionType(String productionType) {
+            this.productionType = productionType;
+        }
+
+        public String getRegDate() {
+            return regDate;
+        }
+
+        public void setRegDate(String regDate) {
+            this.regDate = regDate;
+        }
+
+        public String getRegNumber() {
+            return regNumber;
+        }
+
+        public void setRegNumber(String regNumber) {
+            this.regNumber = regNumber;
+        }
+
+        public List<Product> getProducts() {
+            return products;
+        }
+
+        public void setProducts(List<Product> products) {
+            this.products = products;
+        }
+
+        public Description getDescription() {
+            return description;
+        }
+
+        public void setDescription(Description description) {
+            this.description = description;
+        }
+    }
+
+    public static class Description {
+
+        @JsonProperty("participantInn")
+        private String participantInn;
+
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+    }
+
+
+    public static class Product {
+        @JsonProperty("certificate_document")
+        private String certificateDocument;
+        @JsonProperty("certificate_document_date")
+        private String certificateDocumentDate;
+        @JsonProperty("certificate_document_number")
+        private String certificateDocumentNumber;
+        @JsonProperty("owner_inn")
+        private String ownerInn;
+        @JsonProperty("producer_inn")
+        private String producerInn;
+        @JsonProperty("production_date")
+        private String productionDate;
+        @JsonProperty("tnved_code")
+        private String tnvedCode;
+        @JsonProperty("uit_code")
+        private String uitCode;
+        @JsonProperty("uitu_code")
+        private String uituCode;
+
+        public String getCertificateDocument() {
+            return certificateDocument;
+        }
+
+        public void setCertificateDocument(String certificateDocument) {
+            this.certificateDocument = certificateDocument;
+        }
+
+        public String getCertificateDocumentDate() {
+            return certificateDocumentDate;
+        }
+
+        public void setCertificateDocumentDate(String certificateDocumentDate) {
+            this.certificateDocumentDate = certificateDocumentDate;
+        }
+
+        public String getCertificateDocumentNumber() {
+            return certificateDocumentNumber;
+        }
+
+        public void setCertificateDocumentNumber(String certificateDocumentNumber) {
+            this.certificateDocumentNumber = certificateDocumentNumber;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getTnvedCode() {
+            return tnvedCode;
+        }
+
+        public void setTnvedCode(String tnvedCode) {
+            this.tnvedCode = tnvedCode;
+        }
+
+        public String getUitCode() {
+            return uitCode;
+        }
+
+        public void setUitCode(String uitCode) {
+            this.uitCode = uitCode;
+        }
+
+        public String getUituCode() {
+            return uituCode;
+        }
+
+        public void setUituCode(String uituCode) {
+            this.uituCode = uituCode;
+        }
+    }
+}
